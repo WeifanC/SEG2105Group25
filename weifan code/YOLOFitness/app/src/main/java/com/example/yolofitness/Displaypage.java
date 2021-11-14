4a08048381e4ff63b2f8e343652fc7e3a0c6e648
Louis-Sakuya <90642012+Louis-Sakuya@users.noreply.github.com>
2021-11-14 16:43:13 -0500

    welcome page update

--- weifan code/YOLOFitness/app/src/main/java/com/example/yolofitness/Displaypage.java
+++ weifan code/YOLOFitness/app/src/main/java/com/example/yolofitness/Displaypage.java
@@ -3,10 +3,13 @@ package com.example.yolofitness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Displaypage extends AppCompatActivity {
+    public String ID,identity;
+    public TextView msg_welcome;
 
 
 
@@ -14,6 +17,12 @@ public class Displaypage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_displaypage);
+        Bundle bundle = getIntent().getExtras();
+        ID = bundle.getString("user");
+        identity = bundle.getString("identify");
+        msg_welcome = (TextView) findViewById(R.id.textView2);
+        msg_welcome.setText("Welcome "+ ID + ", You are now successfully sign in as "+ identity+".");
+
 
     }
 }
