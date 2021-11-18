7895a4bdb6dde399c96095494cd2633f2dbfe384
CaesarZJOJO <92767594+CaesarZJOJO@users.noreply.github.com>
2021-11-19 04:02:20 +0800

    Update ClassModel.java

--- weifan code/YOLOFitness/app/src/main/java/com/example/yolofitness/ClassModel.java
+++ weifan code/YOLOFitness/app/src/main/java/com/example/yolofitness/ClassModel.java
@@ -5,11 +5,16 @@ public class ClassModel {
     private String name;
     private String description;
     private String firstname;
+    private String time;
+    private int difficulty;
 
-    public ClassModel(int id, String name, String description) {
+
+    public ClassModel(int id, String name, String description,int difficulty,String time) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.difficulty=difficulty;
+        this.time=time;
     }
 
     @Override
@@ -32,6 +37,14 @@ public class ClassModel {
     public String getDescription() {
         return description;
     }
+
+    public int getDifficulty(){return difficulty;}
+
+    public void setDifficulty(int difficulty){this.difficulty=difficulty;}
+
+    public String getTime(){return time;}
+
+    public void setTime(String time){this.time=time;}
 
     public void setId(int id) {
         this.id = id;
